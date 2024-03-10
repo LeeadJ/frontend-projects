07d52d314b3806a5ec100eab774d2e30937c99f8
Leead Jacobowitz <leead123@gmail.com>
2024-03-10 22:50:24 +0200

    calculator logic works

--- Project:Calculater/calc.js
+++ Project:Calculater/calc.js
@@ -1,24 +1,76 @@
-let add = (num1, num2) => num1 + num2;
-let subtract = (num1, num2) => num1 - num2;
-let multiply = (num1, num2) => num1 * num2;
-let divide = (num1, num2) => (num1 / num2);
-
-let num1, num2, oper;
-
-let operator = (num1, num2, oper) => {
-    switch (oper){
+const operator = (num1, num2, oper) => {
+    if (num2 === '0') {
+        screen.style.cssText = 'color: red; font-size: 24px'
+        return "Error! can't divide by zero"
+    }
+    num1 = +num1;
+    num2 = +num2;
+    switch (oper) {
         case '+':
-            return add(num1, num2);
+            return num1 + num2;
         case '-':
-            return subtract(num1, num2);
+            return num1 - num2;
         case '*':
-            return multiply(num1, num2);
+            return num1 * num2;
         case '/':
-            return divide(num1, num2);
+            return num1 / num2;
         default:
-            console.log("Invalid operator");
             return NaN;
     }
 }
 
-console.log(operator(90,4,'+'));
+function resetCalculator() {
+    num1 = '';
+    num2 = '';
+    oper = '';
+    screen.textContent = '0';
+    screen.style.cssText = 'color: white; font-size: 64px'
+}
+
+let num1 = '';
+let num2 = '';
+let oper = '';
+
+const screen = document.querySelector('#screen');
+screen.textContent = '0';
+
+const numberBtn = document.querySelectorAll('.numberBtn');
+const operatorBtn = document.querySelectorAll('.operatorBtn');
+
+numberBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonText = button.textContent;
+        if (oper === '') {
+            num1 += buttonText;
+        } else {
+            num2 += buttonText;
+        }
+        screen.textContent = oper === '' ? num1 : num2;
+    });
+});
+
+
+operatorBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        buttonText = button.textContent;
+
+        if (buttonText === 'AC') {
+            resetCalculator();
+        } else if (num1 === '' && num2 === '') {
+            // case2: clicked operator before numbers:
+        } else if (num1 !== '' && num2 !== '' && oper !== '') {
+            num1 = String(operator(num1, num2, oper));
+            screen.textContent = num1;
+            oper = buttonText;
+            num2 = '';
+        } else if (buttonText === '=') {
+            if (num1 !== '' && num2 !== '') {
+                num1 = String(operator(num1, num2, oper));
+                screen.textContent = num1;
+                num2 = '';
+            }
+        } else {
+            oper = button.textContent;
+        }
+    })
+})
